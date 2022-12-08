1bacb9fb08620d3d3a86340ff9d17c5979614dbe
Filipe Guedes <filipe.guedes@wmw.com.br>
2022-12-08 16:20:15 -0300

    foi Implementado mais um teste

--- src/test/java/br/com/projeto/integrador/repository/ClienteRepositoryTest.java
+++ src/test/java/br/com/projeto/integrador/repository/ClienteRepositoryTest.java
@@ -1,15 +1,5 @@
 package br.com.projeto.integrador.repository;
 
-import static org.junit.Assert.fail;
-
-import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.net.http.HttpResponse.BodyHandlers;
-import java.util.List;
-
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
